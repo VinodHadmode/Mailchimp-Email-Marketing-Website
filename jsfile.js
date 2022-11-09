c02c0c7b153b7c5266c73dc6dde7dbe17b1399e5
VinodHadmode <vinodhadmode@gmail.com>
2022-11-09 23:36:03 +0530

    Completed day-2 work

---
+++ jsfile.js
@@ -0,0 +1,12 @@
+// let subscription=[
+//     {
+//         type:"Premium",
+//         details:"our most advanced tools,unlimited contacts,priority support;built for teams",
+//         price:2300,
+//         benifits:[ Phone support]
+         
+        
+           
+        
+//     }
+// ]
